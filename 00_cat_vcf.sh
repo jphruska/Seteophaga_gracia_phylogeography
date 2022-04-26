b15a1c369fe30db363a0fd0061f3d3084b2fa867
Jack Hruska <jph239@cornell.edu>
2022-04-26 16:27:22 -0500

    Create 00_cat_vcf.sh

---
+++ 00_cat_vcf.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=cat_vcf
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --partition quanah
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-25
+
+chr_array=$( head -n${SLURM_ARRAY_TASK_ID} vcf_cat.txt | tail -n1 )
+
+chr_start=${chr_array}a.g.vcf
+
+chr_output=${chr_array%__}.g.vcf
+
+grep "#" $chr_start > $chr_output
+
+for i in $( ls $chr_array*vcf ); do grep -v "#" $i >> $chr_output; done
