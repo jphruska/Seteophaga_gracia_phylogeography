791e9de0ffe0f3af3a8d95c14f9e0065baaa1ac8
Jack Hruska <jph239@cornell.edu>
2022-04-27 16:54:02 -0500

    Update 02_run_rohan.sh

--- Rohan/02_run_rohan.sh
+++ Rohan/02_run_rohan.sh
@@ -11,6 +11,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jmanthey/anaconda2/envs/rohan/lib/
 
 basename_array=$( head -n${SLURM_ARRAY_TASK_ID} dataset6.keep.txt | tail -n1 )
 
-/lustre/work/jmanthey/rohan/src/rohan --tstv 1.971 --size 50000 --auto chromosomes.txt -t 8 -o ${SGE_TASK_ID} \
+/lustre/work/jmanthey/rohan/src/rohan --tstv 1.971 --size 50000 --auto chromosomes.txt -t 8 -o ${basename_array} \
 /lustre/scratch/johruska/central_america_pine_oak/parulidae_ref/GCA_001746935.2_mywa_2.1_genomic.fna \
 /lustre/scratch/johruska/setophaga/01_bam_files/${basename_array}_final.bam
