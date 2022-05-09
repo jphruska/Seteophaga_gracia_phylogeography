5b6614e5489ba82424bd4b24700412558193c187
Jack Hruska <jph239@cornell.edu>
2022-05-09 12:24:51 -0500

    Update sum_trees.sh

--- Phylogenies/sum_trees.sh
+++ Phylogenies/sum_trees.sh
@@ -1,1 +1,6 @@
+# Collapse nodes with support < 5 %.
 sumtrees.py --output=setophaga_summed_50kbp.tre --min-clade-freq=0.05 setophaga_total_50kbp.trees 
+
+# Collapse nodes with support < 2.5 %. 
+
+sumtrees.py --output=setophaga_summed_50kbp_2.5_min_clade.tre --min-clade-freq=0.025 setophaga_total_50kbp.trees
