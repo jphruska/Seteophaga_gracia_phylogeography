1eec21ce1ad1aee46bb77d90fafe34137d60c4ab
Jack Hruska <jph239@cornell.edu>
2023-04-12 16:31:06 -0500

    Add files via upload

---
+++ MSMC/msmc_scripts/Setophaga_graciae_KU_33633_RACN_multihetsep.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=Setophaga_graciae_KU_33633_RACN_multihetsep
+#SBATCH --nodes=1 --ntasks=8
+#SBATCH --partition quanah
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=10G
+#SBATCH --array=1-29
+
+input=$(head -n${SLURM_ARRAY_TASK_ID} msmc_chromosomes.txt  | tail -n1 )
+
+/lustre/work/johruska/msmc-tools/generate_multihetsep.py --mask=/lustre/scratch/johruska/central_america_pine_oak/parulidae_ref/masks/setophaga_${input}.mask.35.50.bed.gz --mask=./Setophaga_graciae_KU_33633_RACN/beds/${input}_mask.bed.gz Setophaga_graciae_KU_33633_RACN/vcfs/${input}.vcf.gz > ./Setophaga_graciae_KU_33633_RACN/multihetsep/${input}.txt
