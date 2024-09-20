b9140b88a80494f85a292b87ff7cd039a468ef3f
Jack Hruska <jackphruska@gmail.com>
2024-09-20 17:01:20 -0500

    Add files via upload

---
+++ demographic_history/depth_filtered_run/Setophaga_graciae_KU_8199_SANA_msmc_bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=Setophaga_graciae_KU_8199_SANA_msmc_bootstrap
+#SBATCH --nodes=1 --ntasks=8
+#SBATCH --partition quanah
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=10G
+#SBATCH --array=1-20
+
+/lustre/work/johruska/msmc_2.0.0_linux64bit -o ./Setophaga_graciae_KU_8199_SANA/msmc_output/bootstraps/bootstrap_${SLURM_ARRAY_TASK_ID} -t 8 -i 20  -p 1*2+20*1+1*2+1*3 ./Setophaga_graciae_KU_8199_SANA/multihetsep/bootstraps/bootstrap_${SLURM_ARRAY_TASK_ID}/*.txt -m 0.002401195 -I 0,1
---
+++ demographic_history/depth_filtered_run/Setophaga_graciae_UWBM_95850_ARI_msmc_bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=Setophaga_graciae_UWBM_95850_ARI_msmc_bootstrap
+#SBATCH --nodes=1 --ntasks=8
+#SBATCH --partition quanah
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=10G
+#SBATCH --array=1-20
+
+/lustre/work/johruska/msmc_2.0.0_linux64bit -o ./Setophaga_graciae_UWBM_95850_ARI/msmc_output/bootstraps/bootstrap_${SLURM_ARRAY_TASK_ID} -t 8 -i 20  -p 1*2+20*1+1*2+1*3 ./Setophaga_graciae_UWBM_95850_ARI/multihetsep/bootstraps/bootstrap_${SLURM_ARRAY_TASK_ID}/*.txt -m 0.002603244 -I 0,1
