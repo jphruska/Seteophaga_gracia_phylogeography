a42a19d2fa7b1a7a16df10e21d4638814f0d2cd8
Jack Hruska <jackphruska@gmail.com>
2024-09-20 18:19:29 -0500

    Add files via upload

---
+++ reich's_fst_snps/01_dataset15a_admixture_filtering.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --chdir=./
+#SBATCH --job-name=setophaga_dataset15a_filtering
+#SBATCH --partition quanah
+#SBATCH --nodes=1 --ntasks=12
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+
+for i in $( ls *.g.vcf); do
+vcftools --vcf $i --remove-indels --keep dataset15.keep.txt --out $i.dataset15a.vcf --maf 0.05 --minDP 5 --max-meanDP 50 --recode --minGQ 20 --minQ 20 --max-missing 1 --min-alleles 2 --max-alleles 2 --max-maf 0.49 --thin 10000;
+done
