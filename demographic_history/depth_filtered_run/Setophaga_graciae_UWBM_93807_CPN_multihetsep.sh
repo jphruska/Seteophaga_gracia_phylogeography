6fc1389a3dc6cf83f5e5b2c2c2b4128a443a9dd5
Jack Hruska <jackphruska@gmail.com>
2024-09-20 17:02:16 -0500

    Add files via upload

---
+++ demographic_history/depth_filtered_run/Setophaga_graciae_UWBM_93807_CPN_multihetsep.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=Setophaga_graciae_UWBM_93807_CPN_multihetsep
+#SBATCH --nodes=1 --ntasks=8
+#SBATCH --partition quanah
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=10G
+#SBATCH --array=1-29
+
+input=$(head -n${SLURM_ARRAY_TASK_ID} msmc_chromosomes.txt  | tail -n1 )
+
+/lustre/work/johruska/msmc-tools/generate_multihetsep.py --mask=/lustre/scratch/johruska/central_america_pine_oak/setophaga_ref/masks/setophaga_${input}.mask.35.50.bed.gz --mask=./Setophaga_graciae_UWBM_93807_CPN/beds/${input}_mask.bed.gz Setophaga_graciae_UWBM_93807_CPN/vcfs/${input}.vcf.gz > ./Setophaga_graciae_UWBM_93807_CPN/multihetsep/${input}.txt
