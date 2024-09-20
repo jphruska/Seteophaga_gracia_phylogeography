da9cfc77ab545979a6446e3acc3a90dd2fc05326
Jack Hruska <jackphruska@gmail.com>
2024-09-20 17:00:24 -0500

    Add files via upload

---
+++ demographic_history/depth_filtered_run/Setophaga_graciae_UWBM_114127_CHIH_msmc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=Setophaga_graciae_UWBM_114127_CHIH_msmc
+#SBATCH --nodes=1 --ntasks=8
+#SBATCH --partition quanah
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=10G
+
+/lustre/work/johruska/msmc_2.0.0_linux64bit -o ./Setophaga_graciae_UWBM_114127_CHIH/msmc_output -t 8 -i 20  -p 1*2+20*1+1*2+1*3 ./Setophaga_graciae_UWBM_114127_CHIH/multihetsep/*.txt -m 0.00263262 -I 0,1
